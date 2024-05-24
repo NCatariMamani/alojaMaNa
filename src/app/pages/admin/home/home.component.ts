2c41a820b5f83b67467715801ea1cb4d5aba45a0
NCatariMamani <128624201+NCatariMamani@users.noreply.github.com>
2024-05-23 22:12:07 -0400

    avances de nuevos componentes

--- src/app/pages/admin/home/home.component.ts
+++ src/app/pages/admin/home/home.component.ts
@@ -2,6 +2,13 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { BasePage } from 'src/app/core/shared';
 import { AFFAIR_COLUMNS } from './columns';
+import { LocalDataSource } from 'ng2-smart-table';
+import { BehaviorSubject, takeUntil } from 'rxjs';
+import {
+  ListParams,
+  ListParamsFather,
+  SearchFilter,
+} from 'src/app/common/repository/interfaces/list-params';
 
 @Component({
   selector: 'app-home',
@@ -10,10 +17,13 @@ import { AFFAIR_COLUMNS } from './columns';
   ]
 })
 export class HomeComponent extends BasePage implements OnInit {
-
+  data: LocalDataSource = new LocalDataSource();
+  params = new BehaviorSubject<ListParamsFather>(new ListParams());
+  data1: any = [];
+  totalItems: number = 0;
   constructor(
     private router: Router
-  ) { 
+  ) {
     super();
     this.settings = {
       ...this.settings,
@@ -30,11 +40,76 @@ export class HomeComponent extends BasePage implements OnInit {
   }
 
   ngOnInit(): void {
+    this.openData();
+    this.data.load(this.data1);
+    this.data.refresh();
+    this.loading = false;
+    this.totalItems = this.data1.length;
   }
 
 
-  openForm(){
-    
+  openForm() {
+
+  }
+
+  openData(){
+    this.data1 = [
+      {
+        id: 1,
+        description: 'hola mundo',
+        processDetonate: 'pruebas'
+      },
+      {
+        id: 2,
+        description: 'hola mundo',
+        processDetonate: 'pruebas'
+      },{
+        id: 1,
+        description: 'hola mundo',
+        processDetonate: 'pruebas'
+      },{
+        id: 3,
+        description: 'hola mundo',
+        processDetonate: 'pruebas'
+      },{
+        id: 4,
+        description: 'hola mundo',
+        processDetonate: 'pruebas'
+      },{
+        id: 5,
+        description: 'hola mundo',
+        processDetonate: 'pruebas'
+      },{
+        id: 6,
+        description: 'hola mundo',
+        processDetonate: 'pruebas'
+      },{
+        id: 7,
+        description: 'hola mundo',
+        processDetonate: 'pruebas'
+      },{
+        id: 8,
+        description: 'hola mundo',
+        processDetonate: 'pruebas'
+      },{
+        id: 9,
+        description: 'hola mundo',
+        processDetonate: 'pruebas'
+      },{
+        id: 10,
+        description: 'hola mundo',
+        processDetonate: 'pruebas'
+      },{
+        id: 11,
+        description: 'hola mundo',
+        processDetonate: 'pruebas'
+      },
+      {
+        id: 12,
+        description: 'hola mundo',
+        processDetonate: 'pruebas'
+      }
+    ]
   }
 
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,22 +2,77 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CardComponent } from './components/card/card.component';
 import { ColumnsSelectComponent } from './components/columns-select/columns-select.component';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { Ng2SmartTableModule } from 'ng2-smart-table';
+import { PaginationComponent } from './components/pagination/pagination.component';
+import { PaginateComponent } from './components/pagination/paginate.component';
+import { PaginationModule } from 'ngx-bootstrap/pagination';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
+import { NgxCurrencyModule } from 'ngx-currency';
+import {
+  BsDatepickerConfig,
+  BsDatepickerModule,
+  BsLocaleService,
+} from 'ngx-bootstrap/datepicker';
+import { defineLocale } from 'ngx-bootstrap/chronos';
+import { CustomDatepickerEs } from './utils/CustomDatepickerEs';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+import { NgSelectModule } from '@ng-select/ng-select';
 
+export const customCurrencyMaskConfig = {
+  align: 'right',
+  allowNegative: true,
+  allowZero: false,
+  decimal: '.',
+  precision: 2,
+  prefix: '',
+  suffix: '',
+  thousands: ',',
+  nullable: true,
+};
 
 @NgModule({
   declarations: [
     CardComponent,
-    ColumnsSelectComponent
+    ColumnsSelectComponent,
+    PaginationComponent,
+    PaginateComponent,
   ],
   imports: [
     CommonModule,
+    BsDropdownModule,
+    FormsModule,
     ReactiveFormsModule,
-    
+    NgSelectModule,
+    Ng2SmartTableModule,
+    PaginationModule,
+    NgScrollbarModule,
+    BsDatepickerModule.forRoot(),
+    NgxCurrencyModule.forRoot(customCurrencyMaskConfig)
+
   ],
   exports: [
     CardComponent,
-    ColumnsSelectComponent
+    ColumnsSelectComponent,
+    Ng2SmartTableModule,
+    PaginationComponent,
+    BsDropdownModule,
+    NgxCurrencyModule,
+    NgScrollbarModule,
+    BsDatepickerModule,
+    FormsModule,
+    NgSelectModule
   ]
 })
-export class SharedModule { }
+export class SharedModule {
+  constructor(private localeService: BsLocaleService) {
+    defineLocale('es', CustomDatepickerEs);
+    this.localeService.use('es');
+  }
+}
+
+export function getDatepickerConfig(): BsDatepickerConfig {
+  return Object.assign(new BsDatepickerConfig(), {
+    showWeekNumbers: false,
+  });
+}
