3623a4b01c4a24b1a10963180c75cc972bc00e9a
NCatariMamani <128624201+NCatariMamani@users.noreply.github.com>
2025-01-29 21:57:03 -0400

    configuracion_environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://ncatarimamani.github.io/alojaMaNa/',
-  tokenUrl: 'https://ncatarimamani.github.io/alojaMaNa/auth/login',
-  /*apiUrl: 'http://localhost:3000/',
-  tokenUrl: 'http://localhost:3000/auth/login'*/ 
+  /*apiUrl: 'https://ncatarimamani.github.io/alojaMaNa/',
+  tokenUrl: 'https://ncatarimamani.github.io/alojaMaNa/auth/login',*/
+  apiUrl: 'http://localhost:3000/',
+  tokenUrl: 'http://localhost:3000/auth/login'
 };
