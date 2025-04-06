70127f4b1545d36eb42578b48e01348a1f6cc19d
NCatariMamani <128624201+NCatariMamani@users.noreply.github.com>
2025-04-05 23:11:54 -0400

    Fix_environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,8 @@
 export const environment = {
   production: true,
-  /*apiUrl: 'https://ncatarimamani.github.io/alojaMaNa/',
-  tokenUrl: 'https://ncatarimamani.github.io/alojaMaNa/auth/login',*/
-  apiUrl: 'https://proyecto-curso-node-ts.onrender.com/',
-  tokenUrl: 'https://proyecto-curso-node-ts.onrender.com/auth/login'
+  apiUrl: 'https://2bcd-181-188-161-61.ngrok-free.app/',
+  tokenUrl: 'https://2bcd-181-188-161-61.ngrok-free.app/auth/login',
+  
+  /*apiUrl: 'https://proyecto-curso-node-ts.onrender.com/',
+  tokenUrl: 'https://proyecto-curso-node-ts.onrender.com/auth/login'*/
 };
